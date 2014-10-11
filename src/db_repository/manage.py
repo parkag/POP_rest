1a40efdcc51fe437b9ca543a4c1e709722e6a4fa
parkag <grzegorz.parka@gmail.com>
2014-10-12 01:35:24 +0200

    clean up

--- src/db_repository/manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(six='<module 'six' from '/home/greg/.virtualenvs/pop_rest/lib/python2.7/site-packages/six.pyc'>')
